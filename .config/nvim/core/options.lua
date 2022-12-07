c4278fbd0502620c4c7935667099c66bb7631456
Airscript <dev.airscript@gmail.com>
2022-12-07 01:32:28 +0100

    feat(nvim): add options inside core folder

---
+++ .config/nvim/core/options.lua
@@ -0,0 +1,13 @@
+vim.o.mouse = "a"
+vim.o.title = true
+vim.o.cmdheight = 1
+vim.o.number = true
+vim.o.showcmd = true
+vim.o.shiftwidth = 2
+vim.o.history = 10000
+vim.o.undofile = true
+vim.o.autoindent = true
+vim.o.clipboard = "unnamed"
+vim.o.undodir = "~/.vim/undo"
+vim.o.directory = "~/.vim/swap"
+vim.o.backspace = "indent,eol,start"
